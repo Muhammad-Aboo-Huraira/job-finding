2b84524e9a5c033bff503ccefc557206f7daccf7
Huraira <aboo.g20497@iqra.edu.pk>
2025-11-08 01:22:19 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy, Suspense } from 'react';
+
+const FindJobs = lazy(() => import('./pages/FindJobs'));
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <FindJobs />
+    </Suspense>
   );
 }
 
---
+++ src/pages/FindJobs/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from "./FindJobs.module.css"
+
+export default function FindJobs() {
+  return (
+    <div>
+      <p>Hello</p>
+    </div>
+  )
+}
